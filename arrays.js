cd9a7f507af63dc322a4d3a801b3bc28a7d53ac2
Keginold Philip <keginoldp@hotmail.com>
2019-06-20 15:33:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   return chocolateBars;
 }
 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
