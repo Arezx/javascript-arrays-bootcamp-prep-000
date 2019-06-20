044684d2e049d97fdd987f4577c1a5d4cd54785e
Keginold Philip <keginoldp@hotmail.com>
2019-06-20 15:07:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var foo = 'bar'
+var candystring = 'foo'
 function = addElementToBeginningOfArray(snickers, hundred grand){
   return [hundred grand, ... snickers];
 }
