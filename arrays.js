e4abe7da7bfef099ee87b79c4e4215497bfc8173
Keginold Philip <keginoldp@hotmail.com>
2019-06-20 15:25:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,7 @@ function addElementToEndOfArray(chocolateBars, candyString){
   return [...chocolateBars, candyString];
 }
 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
